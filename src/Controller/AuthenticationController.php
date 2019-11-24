50e2442dab3d068ffe63ead95ff81760e7bffcc1
willypelz <pelumiasefon@gmail.com>
2019-11-24 14:48:54 +0100

    getting users from database

--- src/Controller/AuthenticationController.php
+++ src/Controller/AuthenticationController.php
@@ -7,9 +7,19 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Validator\Constraints\Json;
 use Symfony\Component\HttpFoundation\Request;
+//importing the repository
+use App\Repository\UserRepository;
 
 class AuthenticationController extends AppController
 {
+
+    private $userRepository;
+
+    public function __construct( UserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
     /**
      * @Route("login", name="mac_login")
      */
@@ -25,14 +35,23 @@ class AuthenticationController extends AppController
 
     public function register(Request $request)
     {
-      dd($request->request->all());
-//        return $this->response([
-//            'first_name' => 'michael',
-//            'last_name' => 'pelumi',
-//            'message' => 'users details successfully fetched',
-//            'status_code' => JsonResponse::HTTP_OK
-//        ], JsonResponse::HTTP_OK);
+        $data = [
+            'first_name' => 'michael',
+            'last_name' => 'pelumi',
+            'message' => 'users details successfully fetched',
+            'status_code' => JsonResponse::HTTP_OK
+        ];
+
+        return $this->response($data, JsonResponse::HTTP_OK);
 
     }
+    /**
+     * @Route("getusers", name="get_users")
+     */
+
+    public function getUsers(){
+      $users = $this->userRepository->findAll();
+       dd($users);
+     }
 
 }
